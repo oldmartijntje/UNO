64ddc0a14351ded72c965fc87cdce7c95de35ecc
oldmartijntje <oldmartijntje@gmail.com>
2021-11-18 10:58:28 +0100

    card deck ID's wit corresponding names

--- uno.py
+++ uno.py
@@ -11,7 +11,11 @@ class player(): #player and computer
         self.effect = statusEffects
         self.memory = memory
 
-
+#tells u there is a problem with your config
+def pleaseFixTheConfigFile(exception):
+    input(f"There is a problem with \"{ownPath}/Config.ini\" Go fix it, or Delete it\nPress Enter to close")
+    print(exception)
+    exit()
 
 #get the programs path
 ownPath = pathlib.Path().resolve()
@@ -30,7 +34,7 @@ def createConfig(ownPath):#creates the config file
         settings.write("\n#\n#\n#BE AWARE THAT \"True\" AND \"False\" NEED TO HAVE THE FIRST LETTER CAPITALIZED\n#\n#")
         settings.write("\n#if you want to use a seed, put it here, else, make it False\nFalse")
         settings.write("\n#choose a gamemode, these are the gamemodes:\n#Easy\n#Normal\n#Impossible\n#Exercise\nExercise")
-        settings.write("\n#choose the amount of normal cards (how many times a set of 13 cards x color) default is 2\n2")
+        settings.write("\n#choose the amount of normal cards (how many times a set of 13 cards x color) default is 1\n1")
         settings.write("\n#choose the amount of special cards (4 by default)\n4")
     settings.close()
 
@@ -45,14 +49,11 @@ def readConfig():#reads the config file lines and ignores # lines
     settingsWithoutComments = list()
     #for all lines, if the line starts with a # ignore it, else, add it to the list with settings
     for x in range(len(settingsSplitted)):
-        if settingsSplitted[x][0] != "#":
-                settingsWithoutComments.append(settingsSplitted[x])
         try:
             if settingsSplitted[x][0] != "#":
                 settingsWithoutComments.append(settingsSplitted[x])
-        except:
-            input(f"There is a problem with \"{ownPath}/Config.ini\" Go fix it, or Delete it\nPress Enter to close")
-            exit()
+        except Exception as e:
+            pleaseFixTheConfigFile(e)
     return settingsWithoutComments
 
 def rawSettingsToSettings(rawSettings): #turns settings into settings the program can use
@@ -78,17 +79,16 @@ def rawSettingsToSettings(rawSettings): #turns settings into settings the progra
         if rawSettings[2] != "":#check amount of normal cards
             try:
                 settings.append(int(rawSettings[2]))
-            except:
-                settings.append(2)
+            except Exception as e:
+                pleaseFixTheConfigFile(e)
         if rawSettings[3] != "":#check amount of special cards
             try:
                 settings.append(int(rawSettings[3]))
-            except:
-                settings.append(4)
+            except Exception as e:
+                pleaseFixTheConfigFile(e)
         return settings
-    except:
-        input(f"There is a problem with \"{ownPath}/Config.ini\" Go fix it, or Delete it\nPress Enter to close")
-        exit()
+    except Exception as e:
+        pleaseFixTheConfigFile(e)
 
 def stringToSeed(string): #turns everything into ther ASCII value
     seedList = []
@@ -101,24 +101,44 @@ def stringToSeed(string): #turns everything into ther ASCII value
 def setupCardPile(color, types, special, settings): #shuffles and creates card deck
     cardPile = list()
     for y in range(settings[2]):#creates amount of color cards
-        for x in range(len(color)):
+        for x in range(len(color)-1):
             for i in range(len(types)):
-                cardPile.append(f"{color[x]} {types[i]}")
+                cardPile.append(f"{color[x]}.{types[i]}")
     for z in range(settings[3]):#creates special cards
         for x in range(len(special)):
-            cardPile.append(f"{special[x]}")
+            cardPile.append(f"{color[4]}.{special[x]}")
     random.shuffle(cardPile)
     return cardPile
+
+def cardIdToName(ID):
+    card = ""
+    splitted = ID.split(".")
+    splitted[0] = int(splitted[0])
+    splitted[1] = int(splitted[1])
+    if splitted[0] == 4:
+        card = f"{specialsNames[splitted[1]]}"
+    else:
+        card = f'{cardColorsNames[splitted[0]]} {cardTypesNames[splitted[1]]}'
+    return card
 
 #config thingys
 createConfig(ownPath)
 setting = rawSettingsToSettings(readConfig())
 if setting[0] != False: random.seed(setting[0])#set seed if seed in config
 
+print(setting)
+
 #basic settings
-cardTypes = ["0", '1', '2', '3', '4', '5', '6', '7', '8', '9', 'skip', 'draw two', 'reverse']#all types of cards with a color
-cardColors = ["red", 'blue', 'green', 'yellow']#all colors
-specials = ["wild", 'draw four']#the special cards
+cardTypes = [0, 1, 1, 2, 2, 3, 3, 4, 4, 5, 5, 6, 6, 7, 7, 8, 8, 9, 9, 10, 10, 11, 11, 12, 12]#all types of cards with a color 0-9 numbers, 10 skip, 11 +2, 12 reverse
+cardTypesNames = ["0", '1', '2', '3', '4', '5', '6', '7', '8', '9', 'skip', 'draw two', 'reverse']
+cardColors = [0, 1, 2, 3, 4]#all colors, special, red, blue, green, yellow
+cardColorsNames = ["red", 'blue', 'green', 'yellow', 'black']
+specials = [0, 1]#the special cards, wild, draw 4
+specialsNames = ["wild", 'draw four']
 
 #creating a game
 cardDeck = setupCardPile(cardColors, cardTypes, specials, setting)
+print(cardDeck)
+for x in range(len(cardDeck)):
+    cardDeck[x] = cardIdToName(cardDeck[x])
+print(cardDeck)
