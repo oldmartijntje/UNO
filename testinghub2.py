eefa94aab5e169c24d9132025756b662e5afb5a7
oldmartijntje <oldmartijntje@gmail.com>
2021-12-12 16:28:28 +0100

    cleanup

--- testinghub2.py
+++
@@ -1,8 +0,0 @@
-# Load the dictionary back from the pickle file.
-import pickle
-
-returned = pickle.load( open( "save.p", "rb" ) )
-favorite_color,colorblindNames,computerNameList,specialsNames,specials,cardColorsNames,colorblindCardColorsNames,cardColors, cardTypesNames = returned
-print(returned)
-print(favorite_color)
-# favorite_color is now { "lion": "yellow", "kitty": "red" }
--- testinghub3 copy.py
+++
@@ -1,6 +0,0 @@
-import pickle
-returned = pickle.load( open( "test.e", "rb" ) )
-print(returned)
-yeet, yeetus = returned
-yeetus[0] = "no"
-print(yeetus,yeet)
